86b1fd2337bdc3c040e2c81a602a8e102e487cef
Luciano Braga <lucianobragagroairas@gmail.com>
2018-02-23 12:36:56 -0300

    Update player.js
    Remove player credits

--- player.js
+++ player.js
@@ -45,7 +45,7 @@ function montar(){
 		'</audio>' + // #bragaPlayer
 		'<div class="footer">' +
 			'<p>' +
-				'Player desenvolvido por <a target="_blank" href="https://lucianobragaweb.github.io" title="Programador Web Luciano Braga" alt="Programador Web Luciano Braga">Luciano Braga</a>' +
+				// 'Player desenvolvido por <a target="_blank" href="https://lucianobragaweb.github.io" title="Programador Web Luciano Braga" alt="Programador Web Luciano Braga">Luciano Braga</a>' +
 				'<span id="abrir" class="bt"></span>' +
 				'<span id="fechar" class="bt"></span>' +
 			'</p>' +
